f2326859b505b361ad077f67fdc925c648ffa76f
Suvrat <suvratmittal007@gmail.com>
2023-10-24 14:17:24 +0530

    assignment 2 till 7th question

--- loops.java
+++
@@ -1,9 +0,0 @@
-public class loops {
-    // use WHILE loops when UDK the limit of loop
-    // in  DO WHILE  min . ek baar chalega loop before checking for conditions
-    // sc.input().trim().charAt().......trim() will remove everything extra from aage peeche of the string like spaces
-    // charAt(0)....charater at 0 pos
-    // SHORTCUT: fori-> for loop ke liye
-    
-    
-}
