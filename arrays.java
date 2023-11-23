ff48d7b6835240bcb2c4214f9c3f8da13bc63dbe
Suvrat <suvratmittal007@gmail.com>
2023-11-23 08:07:46 +0530

    arrays till 5

--- arrays.java
+++ arrays.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.*;
+
 public class arrays {
     // for (int num : arr).....sout (num  + " ")
             /*this will go on each element of array and print it*/
@@ -20,8 +23,115 @@ public class arrays {
                     // list.add() , list.set(index, towhat), list.remove(index), list.get(index)
                     // arraylist does have a limit....but when limit is reached....new list created....old ones added to new....new elements add to new too
 
+    // CONVERT STRING TO ARRAY.........Arrays.toString(strvariablename.toCharArray())
+
     public static void main(String[] args) {
-        
+
+//        Scanner sc =new Scanner(System.in);
+//        int [] arr = new int[4];
+//        int [] arr1 = new int[4];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i]=sc.nextInt();
+//        }
+
+
+
+
+
+// 2. WAP for concatenation
+
+/*
+        int [] arr2 = new int[arr1.length+arr.length];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr1[i]=sc.nextInt();
+        }
+
+        for (int i = 0; i < (arr.length+arr1.length); i++) {
+            if(i<arr.length){
+                arr2[i]=arr[i];
+            }else{
+                arr2[i]=arr1[i- arr1.length];
+            }
+
+
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " , ");
+
+        }*/
+
+// 3. sum of elements
+
+/*        int sum=0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum=sum+arr[j];
+
+            }
+        arr1[i]=sum;
+        sum=0;
+
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " , ");
+        }*/
+
+// 4. WAP max val of customer
+
+/*        int[][] accounts={{1,5},{7,3},{3,5}};
+        int[] data=new int[accounts.length];
+        int sum=0;
+
+        for (int cstmer = 0; cstmer < accounts.length; cstmer++) {
+            for (int accbal = 0; accbal < accounts[cstmer].length; accbal++) {
+                sum=sum +accounts[cstmer][accbal];
+            }
+            data[cstmer]=sum;
+            sum=0;
+
+        }
+        int max=0;
+
+        for (int cstmer = 0; cstmer < data.length ; cstmer++) {
+
+            System.out.println("Customer"+ (cstmer+1)+" = "+ data[cstmer]);
+            if (data[cstmer]>max){
+                max=data[cstmer];
+            }else{
+                max=data[cstmer];
+            }
+
+        }
+        System.out.println("Max value is "+ max);*/
+
+// 5. WAP to shuffle array
+/*
+
+        int[] nums = {2,5,1,3,4,7};
+        int n=3;
+        int[] arr=new int[2*n];
+
+
+        for (int i = 0; i < n; i++) {
+            arr[i*2]=nums[i];
+            arr[i*2+1]=nums[i+n];
+        }
+        System.out.println(Arrays.toString(arr));*/
+
+
+
+
+
+
+
+
     }
 
 
