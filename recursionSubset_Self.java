d2735e67fd7a4260ea80b94a53258904d33628a6
Suvrat <suvratmittal007@gmail.com>
2024-02-27 08:40:15 +0530

    practise 2 3 more ques

--- recursionSubset_Self.java
+++ recursionSubset_Self.java
@@ -7,8 +7,7 @@ public class recursionSubset_Self {
     public static void main(String[] args) {
 //        System.out.println(skip_apple("bacapplecadah"));
 //        System.out.println(prmtations_arrlist("","abc"));
-        nokia("","23");
-    }
+        dice("",4);    }
 
 
 // 1. SKIP
@@ -110,6 +109,51 @@ public class recursionSubset_Self {
 
 
 
+    }
+
+    static ArrayList<String> nokia_list(String p , String up){
+        ArrayList<String> arr=new ArrayList<>();
+        if (up.isEmpty()){
+            arr.add(p);
+            return arr;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        int ch= up.charAt(0) - '0';          // CONVERTS '2' TO 2
+
+        if (ch>=2 && ch<=6){
+            for (int i = 3*(ch-2); i < 3*(ch-1); i++) {
+
+                ans.addAll(nokia_list(p+(char)('a'+i),up.substring(1)));
+            }
+        }else if (ch==7){
+            for (int i = 15; i < 19; i++) {
+                ans.addAll(nokia_list(p+(char)('a'+i),up.substring(1)));
+            }
+        } else if (ch==8) {
+            for (int i = 19; i < 22; i++) {
+                ans.addAll(nokia_list(p+(char)('a'+i),up.substring(1)));
+
+            }
+        } else if (ch==9) {
+            for (int i = 22; i < 26; i++) {
+                ans.addAll(nokia_list(p +(char) ('a' + i), up.substring(1)));
+
+            }
+        } else {
+            ans.addAll(nokia_list(p + '0', up.substring(1)));
+        }
+        return ans;
+
+    }
+
+    static void dice(String p, int tgt){
+        if (tgt==0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <=6 && i<=tgt; i++) {
+            dice(p+i,tgt-i);
+        }
     }
 
 
