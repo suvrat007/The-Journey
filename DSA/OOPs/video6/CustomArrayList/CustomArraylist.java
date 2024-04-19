aae7fdd4a4acaa48ffbb8800d024a7b7aab50f27
Suvrat <suvratmittal007@gmail.com>
2024-04-19 08:38:03 +0530

    revision

--- DSA/OOPs/video6/CustomArrayList/CustomArraylist.java
+++ DSA/OOPs/video6/CustomArrayList/CustomArraylist.java
@@ -1,12 +1,14 @@
-package OOPs.video5;
+package OOPs.video6.CustomArrayList;
 
-public class vid6 {
+import java.util.Arrays;
+
+public class CustomArraylist {
     // Costum Arraylist
     private int [] data;
     private static int DEFUALT_SIZE= 10;
     private int size= 0;
 
-    public void CustomArraylist(){
+    public CustomArraylist(){
         this.data=new int[DEFUALT_SIZE];
     }
 
@@ -17,12 +19,14 @@ public class vid6 {
         data[size++]=num;
     }
 
+
     private void resize() {
         int[] temp=new int[data.length*2];
 
         for (int i = 0; i < data.length; i++) {
             temp[i]=data[i];
         }
+        data=temp;
     }
 
     private boolean ifFull() {
@@ -37,13 +41,29 @@ public class vid6 {
     public int get(int index){
         return data[index];
     }
-
     public void set(int index, int value){
         data[index]= value;
+    }
+    @Override
+    public String toString() {
+        return "CustomArraylist{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
     }
 
     public static void main(String[] args) {
         CustomArraylist list =new CustomArraylist();
+//        list.add(3);
+//        list.add(5);
+//        list.add(6);
+//
+//        System.out.println(list);
+
+        for (int i = 0; i < 14; i++) {
+            list.add(2*i);
+        }
+        System.out.println(list);
     }
 
 
