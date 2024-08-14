4aedf94173a2a5d5c1ecfdbb1d8461a7ad655ee9
Suvrat <suvratmittal007@gmail.com>
2024-08-14 23:52:51 +0530

    leetcode questions

--- DSA/Grind75/AddTwoNums.java
+++ DSA/Grind75/AddTwoNums.java
@@ -3,13 +3,7 @@ package Grind75;
 import java.util.List;
 
 public class AddTwoNums {
-    public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 //        ListNode head1 = l1;
