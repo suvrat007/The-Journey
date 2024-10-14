ab282e68d01d3ce146ac9cf33e003dff05b40d50
Suvrat <suvratmittal007@gmail.com>
2024-10-14 18:59:52 +0530

    1 leetcode question

--- DSA/Grind75/ThreeSum.java
+++ DSA/Grind75/ThreeSum.java
@@ -1,15 +1,17 @@
 package Grind75;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ThreeSum {
     public ArrayList<ArrayList<Integer>> threeSum(int[] nums) {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        int i =0,j=1,k=3;
+        int i =0,j=i+1,k=j+1;
+        Arrays.sort(nums);
 
         while(k< nums.length){
             ArrayList<Integer> list = new ArrayList<>();
-            if (nums[i] + nums[j] + nums[k] == 0){
+            if (nums[i] + nums[j] + nums[k] == 0 && i != j && i != k && j != k){
                 // add triplet to ans array
                 list.add(i);
                 list.add(j);
@@ -20,19 +22,20 @@ public class ThreeSum {
         }
         while(j< nums.length-1){
             ArrayList<Integer> list = new ArrayList<>();
-            if (nums[i] + nums[j] + nums[k] == 0){
-                // add triplet to ans array
+            if (nums[i] + nums[j] + nums[k] == 0 && i != j && i != k && j != k){
+                // add triplet to list array
                 list.add(i);
                 list.add(j);
                 list.add(k);
             }
+            // add triplet in list to ans
             ans.add(list);
 
             j++;
         }
         while(i< nums.length-2){
             ArrayList<Integer> list = new ArrayList<>();
-            if (nums[i] + nums[j] + nums[k] == 0){
+            if (nums[i] + nums[j] + nums[k] == 0 && i != j && i != k && j != k){
                 // add triplet to ans array
                 list.add(i);
                 list.add(j);
